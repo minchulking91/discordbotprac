346ed61f3ae7f8b57c9f075ca3c05bad262fd8b6
minchulking91 <minchulking91@gmail.com>
2018-01-29 03:07:28 +0900

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json');
+
+client.on('ready', () => {
+    console.log('i\'m ready');
+});
+
+client.on('message', message => {
+    if(message.content === 'ping'){
+        message.reply('pong'); 
+    }
+});
+
+client.on('message', message => {
+    if(message.content === 'whoru'){
+        message.reply('i\'m 수면시간보장\'s bot!'); 
+    }
+});
+
+
+client.login(settings.token);
