22a84734397e531bcd3004bec37c7720fc052ef7
minchulking91 <minchulking91@gmail.com>
2018-02-03 02:03:39 +0900

    add console

--- emoji.js
+++ emoji.js
@@ -6,8 +6,10 @@ module.exports = {
         var regex = /^!emoji (.*)\n?$/g;
         var result = regex.exec(content);
         if(result != null){
-            var emojiString = result[1];
-            return emojiString.replace(/\s/i, ':regional_indicator_$1:');
+            var emojiString = result[1]; //hi
+            var newString = emojiString.replace(/\s/i, ':regional_indicator_$1:');
+            console.log(newString);
+            return newString;
         }
     }
 }
