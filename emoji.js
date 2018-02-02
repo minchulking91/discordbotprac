d3a68e1a55e647101677896789fd82a9726f8365
minchulking91 <minchulking91@gmail.com>
2018-02-03 01:55:50 +0900

    add emoji

---
+++ emoji.js
@@ -0,0 +1,13 @@
+module.exports = {
+    check: function (content) {
+        return content.startsWith('!emoji');
+    },
+    execute: function (contet) {
+        var regex = /^!emoji (.*)\n?$/g;
+        var result = regex.exec(content);
+        if(result != null){
+            var emojiString = result[1];
+            return emojiString.replace(/\s/i, ':regional_indicator_$1:');
+        }
+    }
+}
