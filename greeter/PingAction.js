47109efc6926706687e0759d1f93289815fb9b55
minchulking91 <minchulking91@gmail.com>
2018-02-05 01:49:42 +0900

    no message

--- greeter/PingAction.js
+++ greeter/PingAction.js
@@ -1,9 +1,6 @@
 const GreeterAction = require('./GreeterAction.js');
 
 class PingAction extends GreeterAction{
-    constructor(message){
-        super(mesasge);
-    }
 
     run(){
         this._message.channel.send('pong');
