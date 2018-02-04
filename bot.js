4b755b30923d698178b029e05db11e313f552c84
minchulking91 <minchulking91@gmail.com>
2018-02-04 22:24:28 +0900

    no message

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 const command = require('./command');
 const databaseHelper = require('./databasehelper');
-const EchoAction = require('./greeter/EchoAction');
+const EchoAction = require('./greeter/echo/EchoAction');
 const GreeterFactory = require('.greeter/GreeterFactory');
 
 client.on('ready', () => {
