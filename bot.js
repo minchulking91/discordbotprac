c315d54f1eff160df0a91aa2ed7d1620a2ea1e79
minchulking91 <minchulking91@gmail.com>
2018-02-05 01:29:05 +0900

    add GreeterAction require

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const greeter = require('./greeter/GreeterAction');
 
 var command = require('./command');
 
