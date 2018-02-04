b3abc4a6bcf91205c968f3f7ac6327cd8534e191
minchulking91 <minchulking91@gmail.com>
2018-02-04 22:18:21 +0900

    no message

---
+++ greeter/dev/PingAction.js
@@ -0,0 +1,20 @@
+module.exports = class EchoAction extends GreeterAction {
+    constructor(message) {
+        this._message = message;
+    }
+    
+    static newInstance(message) {
+        if (typeof message === Message) {
+            if (message.author.bot) {
+                return null;
+            }
+            if(message.content === 'ping'){
+                return new PingAction(message);
+            }
+        }
+        return null;
+    }
+    run() {
+        _message.channel.send('pong');
+    }
+}
